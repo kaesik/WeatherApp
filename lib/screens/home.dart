302eca70b7eafcd88826629e8a7abe3aa0e26404
kaesik <108717713+kaesik@users.noreply.github.com>
2024-02-15 00:41:14 +0100

    reorganization code to bloc

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,50 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/bloc/weather_bloc.dart';
+import 'package:weather_app/widgets/cities_drawer.dart';
 import 'package:weather_app/widgets/main_tile.dart';
+import 'package:weather_app/widgets/settings_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: const Text('Wroclaw, Poland'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              child: Text('Drawer'),
+    return FutureBuilder(
+      future: _determinePosition(),
+      builder: (context, snap) {
+        if (snap.hasData) {
+          return BlocProvider<WeatherBloc>(
+            create: (context) =>
+                WeatherBloc()..add(FetchWeather(snap.data as Position)),
+            child: Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              extendBodyBehindAppBar: true,
+              appBar: AppBar(
+                leading: Builder(
+                  builder: (context) {
+                    return IconButton(
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      icon: const Icon(Icons.pin_drop),
+                    );
+                  },
+                ),
+                backgroundColor: Theme.of(context).colorScheme.background,
+                title: const Text('Wroclaw, Poland'),
+                actions: [
+                  Builder(builder: (context) {
+                    return IconButton(
+                      onPressed: () {
+                        Scaffold.of(context).openEndDrawer();
+                      },
+                      icon: const Icon(Icons.settings),
+                    );
+                  }),
+                ],
+              ),
+              drawer: const CitiesDrawer(),
+              endDrawer: const SettingsDrawer(),
+              drawerEdgeDragWidth: 100,
+              body: const SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: 256),
+                    MainTile(),
+                  ],
+                ),
+              ),
             ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {},
+          );
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
-      drawerEdgeDragWidth: 100,
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 256),
-            MainTile(),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
--- lib/widgets/main_tile.dart
+++ lib/widgets/main_tile.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/data/data.dart';
-import 'package:weather_app/models/weather.dart';
-import 'package:weather_app/providers/user_weather.dart';
 
 class MainTile extends StatefulWidget {
   const MainTile({super.key});
@@ -13,39 +10,17 @@ class MainTile extends StatefulWidget {
 }
 
 class _MainTileState extends State<MainTile> {
-  final _weatherData = UserWeather(apiKey);
-  Weather? _weather;
-
-  _fetchWeather() async {
-    String cityName = await _weatherData.getCurrentCity();
-
-    try {
-      final weather = await _weatherData.getWeather(cityName);
-      setState(() {
-        _weather = weather;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchWeather();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(_weather?.cityName ?? 'Loading...'),
-        const Image(
+        Text('Loading...'),
+        Image(
             image: AssetImage('assets/images/l-clear-d.png'),
             width: 256,
             height: 256),
-        const Row(
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
@@ -62,10 +37,9 @@ class _MainTileState extends State<MainTile> {
             ),
           ],
         ),
-        Text('${_weather?.temperature.round()}°C',
-            style: TextStyle(fontSize: 32)),
+        Text('°C', style: TextStyle(fontSize: 32)),
         Text(
-          _weather?.description ?? 'Loading...',
+          'Loading...',
           style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
         ),
       ],
