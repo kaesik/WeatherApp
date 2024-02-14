09784a4df58cc9c3067b0d516bf4836671f3550c
kaesik <108717713+kaesik@users.noreply.github.com>
2024-02-15 00:40:48 +0100

    return to bloc

---
+++ lib/bloc/weather_state.dart
@@ -0,0 +1,24 @@
+part of 'weather_bloc.dart';
+
+@immutable
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherFailure extends WeatherState {}
+
+class WeatherSuccess extends WeatherState {
+  final Weather weather;
+
+  const WeatherSuccess(this.weather);
+
+  @override
+  List<Object> get props => [weather];
+}
