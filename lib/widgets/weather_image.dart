9d5ec360b7bf66e6abb04a4ba54d51b1f974f8e2
kaesik <108717713+kaesik@users.noreply.github.com>
2024-02-19 23:39:00 +0100

    image is changing with the weather condition code

--- lib/widgets/weather_image.dart
+++ lib/widgets/weather_image.dart
@@ -1,57 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/weather_bloc.dart';
 
 class WeatherImage extends StatelessWidget {
   const WeatherImage({
     super.key,
   });
 
-  getDarkLightMode() {
-    if (true) {
-      return 'd';
-    } else {
-      return 'l';
-    }
-  }
-
-  getWeatherIcon(int code) {
-    switch (code) {
-      case >= 200 && < 300:
-        return 'tstorm';
-      case >= 300 && < 400:
-        return 'drizzle';
-      case >= 500 && < 600:
-        return 'rain';
-      case >= 600 && < 700:
-        return 'snow';
-      case >= 700 && < 800:
-        return 'fog';
-      case == 800:
-        return 'clear';
-      case > 800 && <= 804:
-        return 'clouds';
-      default:
-        return 'clear';
-    }
-  }
-
-  getDayNight() {
-    // var hour = DateTime.now().hour;
-    var hour = 19;
-    if (hour >= 6 && hour < 18) {
-      return 'd';
-    } else {
-      return 'n';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    var mode = getDarkLightMode();
-    var weather = getWeatherIcon(800);
-    var dayNight = getDayNight();
-    return Image(
-        image: AssetImage('assets/images/$mode-$weather-$dayNight.png'),
-        width: 256,
-        height: 256);
+    return BlocBuilder<WeatherBloc, WeatherState>(
+      builder: (context, state) {
+        if (state is WeatherInitial) {}
+
+        if (state is WeatherSuccess) {
+          getDarkLightMode() {
+            if (true) {
+              return 'd';
+            } else {
+              return 'l';
+            }
+          }
+
+          getWeatherIcon(int code) {
+            switch (code) {
+              case >= 200 && < 300:
+                return 'tstorm';
+              case >= 300 && < 400:
+                return 'drizzle';
+              case >= 500 && < 600:
+                return 'rain';
+              case >= 600 && < 700:
+                return 'snow';
+              case >= 700 && < 800:
+                return 'fog';
+              case == 800:
+                return 'clear';
+              case == 801:
+                return 'pcloudy';
+              case > 802 && <= 803:
+                return 'mcloudy';
+              case == 804:
+                return 'cloudy';
+              default:
+                return 'clear';
+            }
+          }
+
+          getDayNight() {
+            // var hour = DateTime.now().hour;
+            var hour = 19;
+            if (hour >= 6 && hour < 18) {
+              return 'd';
+            } else {
+              return 'n';
+            }
+          }
+
+          var mode = getDarkLightMode();
+          var weather =
+              getWeatherIcon(state.weather.weatherConditionCode!.toInt());
+          var dayNight = getDayNight();
+          return Image(
+              image: AssetImage('assets/images/$mode-$weather-$dayNight.png'),
+              width: 256,
+              height: 256);
+        } else if (state is WeatherLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is WeatherFailure) {
+          return const Image(
+              image: AssetImage('assets/images/unknown.png'),
+              width: 256,
+              height: 256);
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
