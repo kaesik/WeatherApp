08c90c6e27ec4bc3f564552296325800cc41d4a4
kaesik <108717713+kaesik@users.noreply.github.com>
2024-02-20 23:37:50 +0100

    implement changing theme

--- lib/widgets/settings_drawer.dart
+++ lib/widgets/settings_drawer.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/theme/theme_bloc.dart';
 
-class SettingsDrawer extends StatelessWidget {
+class SettingsDrawer extends StatefulWidget {
   const SettingsDrawer({super.key});
 
+  @override
+  State<SettingsDrawer> createState() => _SettingsDrawerState();
+}
+
+class _SettingsDrawerState extends State<SettingsDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: const <Widget>[
-          ListTile(
-            title: Text('City 1'),
-          ),
-          ListTile(
-            title: Text('City 2'),
-          ),
-          ListTile(
-            title: Text('City 3'),
-          ),
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Dark Mode',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                Switch(
+                  value: context.read<ThemeBloc>().state == ThemeMode.system
+                      ? MediaQuery.of(context).platformBrightness ==
+                          Brightness.dark
+                      : context.read<ThemeBloc>().state == ThemeMode.dark,
+                  onChanged: (value) {
+                    setState(() {
+                      context.read<ThemeBloc>().add(ThemeChanged(value));
+                      print(
+                        MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark ||
+                            context.read<ThemeBloc>().state == ThemeMode.dark,
+                      );
+                    });
+                  },
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
