70f85de3ae7d222533064abe14f19752948cbe29
kaesik <108717713+kaesik@users.noreply.github.com>
2024-02-28 23:48:26 +0100

    changed logic of weather_image.dart

--- lib/widgets/weather_image.dart
+++ lib/widgets/weather_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/image/image_bloc.dart';
 import 'package:weather_app/bloc/theme/theme_bloc.dart';
 import 'package:weather_app/bloc/weather/weather_bloc.dart';
 
@@ -10,63 +11,36 @@ class WeatherImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    getDarkLightMode(bool isDarkMode) {
-      return isDarkMode ? 'd' : 'l';
-    }
-
-    getWeatherIcon(int code) {
-      switch (code) {
-        case >= 200 && < 300:
-          return 'tstorm';
-        case >= 300 && < 400:
-          return 'drizzle';
-        case >= 500 && < 600:
-          return 'rain';
-        case >= 600 && < 700:
-          return 'snow';
-        case >= 700 && < 800:
-          return 'fog';
-        case == 800:
-          return 'clear';
-        case > 800 && <= 802:
-          return 'pcloudy';
-        case > 802 && <= 803:
-          return 'mcloudy';
-        case == 804:
-          return 'cloudy';
-        default:
-          return 'clear';
-      }
-    }
-
-    getDayNight(int hour) {
-      switch (hour) {
-        case >= 6 && < 18:
-          return 'd';
-        default:
-          return 'n';
-      }
-    }
-
     return BlocBuilder<WeatherBloc, WeatherState>(
       builder: (context, state) {
-        if (state is WeatherInitial) {}
-
         if (state is WeatherSuccess) {
-          var mode = getDarkLightMode(
-            MediaQuery.of(context).platformBrightness == Brightness.dark ||
-                context.read<ThemeBloc>().state == ThemeMode.dark,
+          return BlocBuilder<ThemeBloc, ThemeMode>(
+            builder: (contextTheme, stateTheme) {
+              return BlocBuilder<ImageBloc, ImageState>(
+                bloc: ImageBloc()
+                  ..add(FetchImage(
+                    state.weather,
+                    context.read<ThemeBloc>().state == ThemeMode.dark,
+                  )),
+                builder: (context, state) {
+                  if (state is ImageSuccess) {
+                    print(
+                      '/kaesik WeatherImage: ${state.imageUrl}',
+                    );
+                    return Image(
+                        image: AssetImage(state.imageUrl),
+                        width: 256,
+                        height: 256);
+                  } else {
+                    return const Image(
+                        image: AssetImage('assets/images/unknown.png'),
+                        width: 256,
+                        height: 256);
+                  }
+                },
+              );
+            },
           );
-          var weather = getWeatherIcon(
-            state.weather.weatherConditionCode!.toInt(),
-          );
-          var dayNight = getDayNight(
-            state.weather.date!.hour.toInt(),
-          );
-          return Image(
-              image: AssetImage('assets/images/$mode-$weather-$dayNight.png'),
-              width: 256,
-              height: 256);
         } else if (state is WeatherLoading) {
           return const Center(
             child: CircularProgressIndicator(),
