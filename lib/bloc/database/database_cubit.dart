97cd7b60cee565b1a36e0580ed0b68157ad472aa
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-05 01:39:36 +0100

    created database provider

---
+++ lib/bloc/database/database_cubit.dart
@@ -0,0 +1,52 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+part 'database_state.dart';
+
+class DatabaseCubit extends Cubit<DatabaseState> {
+  DatabaseCubit() : super(InitDatabaseState());
+
+  Database? database;
+
+  Future<void> initDatabase() async {
+    final databasePath = await getDatabasesPath();
+    // print(databasePath);
+    final path = join(databasePath, 'cities.db');
+    if (await Directory(dirname(path)).exists()) {
+      database = await openDatabase(path, version: 1,
+          onCreate: (Database db, int version) async {
+        // When creating the db, create the table
+        await db.execute('''CREATE TABLE cities (
+            id INTEGER PRIMARY KEY, 
+            name TEXT, 
+            country TEXT, 
+            latitude REAL, 
+            longitude REAL
+            )''');
+      });
+      emit(LoadDatabaseState());
+    } else {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+        database = await openDatabase(path, version: 1,
+            onCreate: (Database db, int version) async {
+          // When creating the db, create the table
+          await db.execute('''CREATE TABLE cities (
+              id INTEGER PRIMARY KEY, 
+              name TEXT, 
+              country TEXT, 
+              latitude REAL, 
+              longitude REAL
+              )''');
+        });
+        emit(LoadDatabaseState());
+      } catch (e) {
+        log(e.toString());
+      }
+    }
+  }
+}
---
+++ lib/bloc/database/database_state.dart
@@ -0,0 +1,7 @@
+part of 'database_cubit.dart';
+
+abstract class DatabaseState {}
+
+class InitDatabaseState extends DatabaseState {}
+
+class LoadDatabaseState extends DatabaseState {}
