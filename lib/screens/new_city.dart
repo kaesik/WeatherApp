5cdbf37ea0458837e80edf2889f7dd3c7f43b3b4
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-11 22:12:20 +0100

    added more providers

--- lib/screens/new_city.dart
+++ lib/screens/new_city.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/city/city_cubit.dart';
+import 'package:weather_app/bloc/database/database_cubit.dart';
+import 'package:weather_app/models/city.dart';
 
 class NewCityScreen extends StatefulWidget {
   const NewCityScreen({super.key});
@@ -10,27 +14,32 @@ class NewCityScreen extends StatefulWidget {
 class _NewCityScreenState extends State<NewCityScreen> {
   final _formKey = GlobalKey<FormState>();
   var _enteredName = '';
-// PlaceLocation? _selectedLocation;
+  CityCubit? _cityCubit;
+  CityLocation? _selectedLocation;
 
-  // void _savePlace() {
-  //   if (_formKey.currentState!.validate() &&
-  //       _selectedImage != null &&
-  //       _selectedLocation != null) {
-  //     _formKey.currentState!.save();
-  //
-  //     ref
-  //         .read(
-  //           userPlacesProvider.notifier,
-  //         )
-  //         .addPlace(
-  //           _enteredName,
-  //           _selectedImage!,
-  //           _selectedLocation!,
-  //         );
-  //
-  //     Navigator.of(context).pop();
-  //   }
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _cityCubit = context.read<CityCubit>();
+  }
+
+  void _savePlace() {
+    if (_formKey.currentState!.validate() && _selectedLocation != null) {
+      _formKey.currentState!.save();
+
+      if (_cityCubit != null) {
+        _cityCubit!.addCity(
+          // Change from addCities to addCity
+          _enteredName,
+          'Country', // Replace 'Country' with the actual country data
+          _selectedLocation?.latitude ?? 0.0,
+          _selectedLocation?.longitude ?? 0.0,
+        );
+      }
+
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,52 +47,75 @@ class _NewCityScreenState extends State<NewCityScreen> {
       appBar: AppBar(
         title: const Text('Add New City'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
+      body: BlocBuilder<DatabaseCubit, DatabaseState>(
+        builder: (context, state) {
+          if (state is LoadDatabaseState) {
+            return BlocConsumer<CityCubit, CityState>(
+              listener: (context, state) {
+                // Handle CityUpdated or CityLoadFailure events if needed
+              },
+              builder: (context, state) {
+                if (state is CityLoadSuccess) {
+                  return SingleChildScrollView(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onBackground,
+                                    ),
+                            maxLength: 50,
+                            decoration:
+                                const InputDecoration(labelText: 'Name'),
+                            initialValue: _enteredName,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.trim().length <= 1 ||
+                                  value.trim().length > 50) {
+                                return 'Please enter a name. (1-50 characters)';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredName = value!;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton.icon(
+                            onPressed: _savePlace,
+                            icon: const Icon(Icons.add),
+                            label: const Text(
+                              'Add City',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                maxLength: 50,
-                decoration: const InputDecoration(labelText: 'Name'),
-                initialValue: _enteredName,
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
-                    return 'Please enter a name. (1-50 characters)';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _enteredName = value!;
-                },
-              ),
-              const SizedBox(height: 16),
-              const SizedBox(height: 16),
-              // LocationInput(
-              //   onSelectLocation: (location) {
-              //     _selectedLocation = location;
-              //   },
-              // ),
-              const SizedBox(height: 16),
-              ElevatedButton.icon(
-                // onPressed: _savePlace,
-                onPressed: () {},
-                icon: const Icon(Icons.add),
-                label: const Text(
-                  'Add City',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
-          ),
-        ),
+                  );
+                } else if (state is CityLoadFailure) {
+                  return const Center(
+                    child: Text('Failed to load cities'),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            );
+          } else {
+            return const Center(
+              child: Text('Database not loaded'),
+            );
+          }
+        },
       ),
     );
   }
