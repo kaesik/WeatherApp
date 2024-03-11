5b7fd85c978af990345c1020c7b1acc69d3ea9ab
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-11 22:12:53 +0100

    something is fckdup...

--- lib/bloc/database/database_cubit.dart
+++ lib/bloc/database/database_cubit.dart
@@ -10,41 +10,48 @@ part 'database_state.dart';
 class DatabaseCubit extends Cubit<DatabaseState> {
   DatabaseCubit() : super(InitDatabaseState());
 
-  Database? database;
+  Database? _database;
 
+  Database? get database => _database;
+
+  // Initialize the SQLite database
   Future<void> initDatabase() async {
     final databasePath = await getDatabasesPath();
-    // print(databasePath);
     final path = join(databasePath, 'cities.db');
+
+    // Check if the directory exists, create if not
     if (await Directory(dirname(path)).exists()) {
-      database = await openDatabase(path, version: 1,
+      _database = await openDatabase(path, version: 1,
           onCreate: (Database db, int version) async {
-        // When creating the db, create the table
+        // When creating the db, create the 'cities' table
         await db.execute('''
-            CREATE TABLE cities (
+          CREATE TABLE cities (
             id INTEGER PRIMARY KEY, 
             name TEXT, 
             country TEXT, 
             latitude REAL, 
             longitude REAL
-            )''');
+          )''');
       });
+
       emit(LoadDatabaseState());
     } else {
       try {
+        // Create the directory and open the database
         await Directory(dirname(path)).create(recursive: true);
-        database = await openDatabase(path, version: 1,
+        _database = await openDatabase(path, version: 1,
             onCreate: (Database db, int version) async {
-          // When creating the db, create the table
+          // When creating the db, create the 'cities' table
           await db.execute('''
-              CREATE TABLE cities (
+            CREATE TABLE cities (
               id INTEGER PRIMARY KEY, 
               name TEXT, 
               country TEXT, 
               latitude REAL, 
               longitude REAL
-              )''');
+            )''');
         });
+
         emit(LoadDatabaseState());
       } catch (e) {
         log(e.toString());
