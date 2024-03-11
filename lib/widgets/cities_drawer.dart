979051aa7cd418ecbd975cd5758e3adfa6d3a1ef
kaesik <108717713+kaesik@users.noreply.github.com>
2024-03-11 22:10:55 +0100

    added more null safety

--- lib/widgets/cities_drawer.dart
+++ lib/widgets/cities_drawer.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app/bloc/city/city_bloc.dart';
+import 'package:weather_app/bloc/city/city_cubit.dart';
 import 'package:weather_app/models/city.dart';
 import 'package:weather_app/screens/new_city.dart';
 import 'package:weather_app/widgets/city_card.dart';
 
 class CitiesDrawer extends StatelessWidget {
-  const CitiesDrawer({super.key});
+  const CitiesDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,24 +14,33 @@ class CitiesDrawer extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 64),
-          BlocBuilder<CityBloc, CityState>(
+          BlocBuilder<CityCubit, CityState>(
             builder: (context, state) {
-              if (state is CityUpdated && state.cities.isNotEmpty) {
+              if (state is CityLoadSuccess) {
                 final cities = state.cities;
-                return ListView.builder(
-                  itemCount: cities.length,
-                  itemBuilder: (context, index) {
-                    final city = cities[index];
-                    return CityCard(city: city);
-                  },
-                );
+
+                if (cities.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: cities.length,
+                    itemBuilder: (context, index) {
+                      final city = cities[index];
+                      return CityCard(city: city);
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: Text('No cities added'),
+                  );
+                }
               } else {
                 return const Center(
-                  child: Text('No cities added'),
+                  child: Text('City cubit not initialized'),
                 );
               }
             },
           ),
+          // Add padding between the city list and the add button
+          const SizedBox(height: 16),
           IconButton(
             onPressed: () {
               Navigator.of(context).push<City>(
